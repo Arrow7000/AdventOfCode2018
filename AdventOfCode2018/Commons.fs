3d591f29f8532996d06ef24f1cfc5d35db3e70f3
Aron Adler <aron@fundapps.co>
2018-12-05 15:06:14 +0000

    Add Commons utils

---
+++ AdventOfCode2018/Commons.fs
@@ -0,0 +1,24 @@
+﻿module Commons
+
+open System.IO
+open System.Reflection
+
+let inPath file =
+    let sep = Path.DirectorySeparatorChar.ToString()
+    let fullPath = 
+        Assembly.GetCallingAssembly().Location
+        |> Path.GetDirectoryName 
+        |> fun dir -> dir + sep + "inputs" + sep + file
+    fullPath
+
+let getLines =
+    inPath
+    >> File.ReadAllLines 
+    >> Array.toList
+
+let getText =
+    inPath >> File.ReadAllText
+
+let strSplit sep (str : string) =
+    str.Split([| sep |])
+    |> Array.toList
