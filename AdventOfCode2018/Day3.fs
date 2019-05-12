6c83b5056cf38658c28b2025e697d9dca09346d6
Aron <aronadler@hotmail.com>
2019-05-13 00:12:56 +0100

    Add day 3 part 1 + improve Program

---
+++ AdventOfCode2018/Day3.fs
@@ -0,0 +1,64 @@
+﻿// https://adventofcode.com/2018/day/3
+
+module Day3
+
+open FSharp.Text.RegexProvider
+open Utils
+
+type Rect =
+    { Id : int
+      Left : int
+      Top: int
+      Width: int
+      Height: int }
+
+type RectRegex =
+    Regex< @"^#(?<id>\d+) @ (?<left>\d+),(?<top>\d+): (?<width>\d+)x(?<height>\d+)$" >
+
+let lines = getLines "Day3.txt"
+
+
+let getRect line =
+    let r = RectRegex().TypedMatch(line)
+    { Id = int r.id.Value
+      Left = int r.left.Value
+      Top = int r.top.Value
+      Width = int r.width.Value
+      Height = int r.height.Value }
+
+
+let getCoveredInches left top width height =
+    let rows = List.init height (fun i -> top + i)
+
+    rows
+    |> List.map (fun row -> List.init width (fun j -> (row, j + left)))
+    |> List.collect id
+
+
+let getClaims rects =
+    let rec computer coveredInchesCounts remainingRects =
+        match remainingRects with
+        | [] ->
+            coveredInchesCounts
+            |> Map.filter (fun _ v -> v >= 2)
+            |> Map.count
+        | rect :: rest ->
+            let coveredInches =
+                getCoveredInches rect.Left rect.Top rect.Width rect.Height
+            
+            let newCoveredInchesCounts =
+                coveredInches
+                |> List.fold (fun map inch -> 
+                                    match Map.tryFind inch map with
+                                    | Some count -> Map.add inch (count+1) map
+                                    | None -> Map.add inch 1 map) coveredInchesCounts
+
+            computer newCoveredInchesCounts rest
+
+    computer Map.empty rects
+
+
+let part1 =
+    lines
+    |> List.map getRect
+    |> getClaims
--- AdventOfCode2018/Program.fs
+++ AdventOfCode2018/Program.fs
@@ -7,14 +7,18 @@ let applyTriple f (a,b,c) = f a b c
 let printForDay dayNum partNum result =
     printfn "Day %i part %i result is: %A" dayNum partNum result
 
+let printResult dayNum part1 part2 =
+    printForDay dayNum 1 part1
+    printForDay dayNum 2 part2
+
+let inline s x = string x
+
 [<EntryPoint>]
 let main _ =
-    [ [ 1, 1, string Day1.part1
-        1, 2, string Day1.part2 ]
-      [ 2, 1, string Day2.part1
-        2, 2, Day2.part2 ] ]
-    |> List.collect id
-    |> List.iter (applyTriple printForDay)
+    [ s Day1.part1, s Day1.part2
+      s Day2.part1, s Day2.part2
+      s Day3.part1, s "" ]
+    |> List.iteri (fun i (a,b) -> printResult (i+1) a b)
     
     System.Console.ReadKey() |> ignore
     0 // return an integer exit code
